105a8064be3479cfe3e7a726070f5ef5abadad1f
mohamedsabrie <abosaadmohamed@gmail.com>
2021-12-07 17:42:12 +0200

    another tiny fix

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <div className="h-28    w-screen bg-green-800 text-white flex items-center justify-center flex-col font-semibold">
+        <div className="h-28  bg-green-800 text-white flex items-center justify-center flex-col font-semibold">
             <h5 className="text-lg text-white mb-2">هذا ليس  موقع حقيقي </h5>
             <h5 className="text-white">    انه نموذج لعرض العمل </h5>
         </div>
