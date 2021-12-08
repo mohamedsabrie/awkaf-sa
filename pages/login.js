2d46fd48ac19697670d6ec23dcf93ec2b93f196c
mohamedsabrie <abosaadmohamed@gmail.com>
2021-12-08 16:15:26 +0200

    change the head

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,20 @@
-import Footer from "../components/Footer"
-import Login from "../components/Login"
-import Navbar from "../components/Navbar"
+import Footer from "../components/Footer";
+import Login from "../components/Login";
+import Navbar from "../components/Navbar";
+import Head from 'next/head'
+
 
 function LoginPage() {
-    return (
-        <div>
-            <Navbar />
-            <Login />
-        </div>
-    )
+  return (
+    <div>
+      <Head>
+        <title>Login</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <Login />
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
