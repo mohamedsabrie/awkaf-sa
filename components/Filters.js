43ecd3d6d1118694f7bd2d87139df74a97c48532
mohamedsabrie <abosaadmohamed@gmail.com>
2021-12-18 17:44:13 +0200

    enhancements for the branches treeselect

--- components/Filters.js
+++ components/Filters.js
@@ -1,24 +1,27 @@
 import { DatePicker, Table } from "antd";
 import DropDown from "../components/ui/DropDown";
 import Image from "next/image";
-import {UploadIcon} from '@heroicons/react/solid'
+import { UploadIcon } from "@heroicons/react/solid";
 // import { branchesResult } from "./BranchesTree";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Transition } from "@headlessui/react";
 import { Fragment } from "react";
 import Tree from "../components/Tree";
-import { v4 as uuidv4 } from 'uuid';
-import {CheckIcon, ChevronDownIcon, ChevronUpIcon, XIcon} from '@heroicons/react/outline'
-
+import { v4 as uuidv4 } from "uuid";
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+  XIcon,
+} from "@heroicons/react/outline";
 
 const { RangePicker } = DatePicker;
 
-import moment from 'moment';
-import 'moment/locale/ar-sa'
+import moment from "moment";
+import "moment/locale/ar-sa";
 import BranchesTree from "./BranchesTree";
 
-moment.locale('ar-sa');
-
+moment.locale("ar-sa");
 
 const seasonData = [
   {
@@ -35,7 +38,6 @@ const masrafData = [
   {
     title: "مساجد",
     value: "مساجد",
-   
   },
   {
     title: " سكن",
@@ -44,10 +46,7 @@ const masrafData = [
   {
     title: " سقيا",
     value: " سقيا",
-    
   },
-  
- 
 ];
 
 const areaData = [
@@ -100,6 +99,7 @@ function Filters() {
   const branch2 = useRef(null);
   const branch3 = useRef(null);
   const inputRef = useRef(null);
+  const branchesRef = useRef(null);
 
   const handleShowBranches = () => {
     setShowBranches((prev) => !prev);
@@ -139,41 +139,45 @@ function Filters() {
     }
   };
 
-  const removeElement = (v)=>{
+  const removeElement = (v) => {
     setBranchesResult((prev) => prev.filter((item) => item !== v));
-    if(v === "تفرع1"){
-      setBr0("bg-white")
+    if (v === "تفرع1") {
+      setBr0("bg-white");
     }
-    if(v === "تفرع2"){
-      setBr1("bg-white")
+    if (v === "تفرع2") {
+      setBr1("bg-white");
     }
-    if(v === "تفرع3"){
-      setBr2("bg-white")
+    if (v === "تفرع3") {
+      setBr2("bg-white");
     }
+  };
 
-  }
-  
-
-  const ResultData = ()=>{
+  const ResultData = () => {
     return (
-     <div className="flex items-center   h-full ">
-       {branchesResult.map(item => (
-         <div className="flex items-center justify-center px-2 bg-gray-100 mx-1" key= {uuidv4()} >
-           <XIcon className="h-3 text-gray-800 cursor-pointer hover:scale-110 ml-2 transition ease-in-out duration-200 "  onClick={()=> removeElement(item)} />
-           {item}
-           </div>
-       ))}
-     </div>
-    )
-    
-  }
+      <div className="flex items-center   h-full ">
+        {branchesResult.map((item) => (
+          <div
+            className="flex items-center justify-center px-2 bg-gray-100 mx-1"
+            key={uuidv4()}
+          >
+            <XIcon
+              className="h-3 text-gray-800 cursor-pointer hover:scale-110 ml-2 transition ease-in-out duration-200 "
+              onClick={() => removeElement(item)}
+            />
+            {item}
+          </div>
+        ))}
+      </div>
+    );
+  };
 
   const handleReset = () => {
     setSeason(undefined);
     setMasraf(undefined);
     setArea(undefined);
     setDate({});
-    setBranch(undefined)
+    setBranch(undefined);
+    setBranchesResult([]);
   };
 
   const Results = () => {
@@ -211,7 +215,7 @@ function Filters() {
         masref: masraf?.[0],
         season: season?.[0],
         branch: branchesResult?.[0],
-        date: ` البداية: ${date?.start || ""}` ,
+        date: ` البداية: ${date?.start || ""}`,
       },
       {
         key: "2",
@@ -219,7 +223,7 @@ function Filters() {
         masref: masraf?.[1],
         season: season?.[1],
         branch: branchesResult?.[1],
-        date: ` النهاية:  ${date?.end || ""}` ,
+        date: ` النهاية:  ${date?.end || ""}`,
       },
       {
         key: "3",
@@ -252,19 +256,17 @@ function Filters() {
 
   const handleMasraf = (v) => {
     setMasraf(v);
-    console.log(v)
+    console.log(v);
   };
   const handleBranch = (v) => {
     setBranch(v);
   };
   const handleArea = (v) => {
     setArea(v);
-    
   };
 
-
   const handleDateChange = (e) => {
-    const [moment1, moment2]=  e;
+    const [moment1, moment2] = e;
 
     const start = `${moment1?._d?.getDate()}-${
       moment1?._d?.getMonth() + 1
@@ -272,23 +274,39 @@ function Filters() {
     const end = `${moment2?._d?.getDate()}-${
       moment2?._d?.getMonth() + 1
     }-${moment2?._d?.getFullYear()}`;
-    setDate({start, end});
+    setDate({ start, end });
   };
-  console.log(date)
+  console.log(date);
 
   const showResults = () => {
     setShow((prevState) => !prevState);
   };
- 
 
-    
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      // If the menu is open and the clicked target is not within the menu,
+      // then close the menu
+      if (
+        showBranches &&
+        branchesRef.current &&
+        !branchesRef.current.contains(e.target)
+      ) {
+        setShowBranches(false);
+      }
+    };
+
+    document.addEventListener("mousedown", checkIfClickedOutside);
+
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, [showBranches]);
 
   return (
     <div id="filters">
-
       <div className="px-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-10 items-center justify-between max-w-6xl mx-auto py-10 bg-green-500 px-10 rounded-xl my-10 shadow-lg">
-
           <Tree
             handleChange={handleArea}
             value={area}
@@ -301,69 +319,106 @@ function Filters() {
             placeholder="   المصارف"
             data={masrafData}
           />
-          
-     
-     
-     <div className="relative">
-       <span className="absolute flex items-center justify-center right-0 top-0 w-10 h-[28px] bg-white border"  onClick={handleShowBranches}>
-        {showBranches ? <ChevronUpIcon className="h-4" />: <ChevronDownIcon className="h-4" /> } 
-       </span>
-       <div
-         ref={inputRef}
-         placeholder="Branches"
-         className="min-h-[28px] bg-white border  mr-10 flex items-center" 
-       >
-         {branchesResult.length ? <ResultData /> : <span className="text-sm text-gray-500 mr-4">التفرعات</span> }
 
-
-       </div>
-       <Transition
-         as={Fragment}
-         enter="transition ease-out duration-300"
-         enterFrom="transform opacity-0 height-0"
-         enterTo="transform opacity-100 height-100"
-         leave="transition ease-in duration-300"
-         leaveFrom="transform opacity-100 height-100"
-         leaveTo="transform opacity-0 height-0"
-         show={showBranches}
-       >
-         <div className="absolute  z-20 flex flex-col top-full mt-1 left-0 w-full bg-white py-2 px-6">
-           <div className="flex items-center gap-x-2 mb-2 ">
-             <span
-               ref={branch1}
-               onClick={() => handleBranch1Click("تفرع1")}
-           className={`w-4 h-4 rounded-sm ${br0} border border-gray-500 flex items-center justify-center cursor-pointer`}
-             >
-
-               {br0 === "bg-blue-500" ? <CheckIcon className="h-4 text-white " /> : br0 === "bg-red-500"? <XIcon className="h-4" /> : ""}
-             </span>
-             <p>تفرع1</p>
-           </div>
-           <div className="flex items-center gap-x-2  mb-2 ">
-             <span
-               ref={branch2}
-               onClick={() => handleBranch2Click("تفرع2")}
-               className={`w-4 h-4 rounded-sm  ${br1}  border border-gray-500 flex items-center justify-center cursor-pointer`}
-             >
-               {br1 === "bg-blue-500" ? <CheckIcon className="h-4 text-white " /> : br1 === "bg-red-500"? <XIcon className="h-4" /> : ""}
-             </span>
-             <p>تفرع2</p>
-           </div>
-           <div className="flex items-center gap-x-2  mb-2 ">
-             <span
-               ref={branch3}
-               onClick={() => handleBranch3Click("تفرع3")}
-               className={`w-4 h-4 rounded-sm  ${br2}  border border-gray-500 flex items-center justify-center cursor-pointer`}
-             >
-
-{br2 === "bg-blue-500" ? <CheckIcon className="h-4 text-white " /> : br2 === "bg-red-500"? <XIcon className="h-4" /> : ""}
-
-             </span>
-             <p>تفرع3</p>
-           </div>
-         </div>
-       </Transition>
-     </div>
+          <div className="relative" ref={branchesRef}>
+            <span
+              className="absolute flex items-center justify-center right-0 top-0 w-10 h-[28px] bg-white border"
+              onClick={handleShowBranches}
+            >
+              {showBranches ? (
+                <ChevronUpIcon className="h-4 cursor-pointer" />
+              ) : (
+                <ChevronDownIcon className="h-4 cursor-pointer" />
+              )}
+            </span>
+            <div
+              ref={inputRef}
+              placeholder="Branches"
+              className="min-h-[28px] bg-white border  mr-10 flex items-center"
+            >
+              {branchesResult.length ? (
+                <ResultData />
+              ) : (
+                <span className="text-sm text-gray-500 mr-4">التفرعات</span>
+              )}
+            </div>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-300"
+              enterFrom="transform opacity-0 height-0"
+              enterTo="transform opacity-100 height-100"
+              leave="transition ease-in duration-300"
+              leaveFrom="transform opacity-100 height-100"
+              leaveTo="transform opacity-0 height-0"
+              show={showBranches}
+            >
+              <div className="absolute  z-20 flex flex-col top-full mt-1 left-0 w-full bg-white py-2 pr-6 pl-2">
+                <div className="flex items-center gap-x-2  ">
+                  <span
+                    ref={branch1}
+                    onClick={() => handleBranch1Click("تفرع1")}
+                    className={`w-4 h-4 rounded-sm ${br0} border border-gray-500 flex items-center justify-center cursor-pointer`}
+                  >
+                    {br0 === "bg-blue-500" ? (
+                      <CheckIcon className="h-4 text-white " />
+                    ) : br0 === "bg-red-500" ? (
+                      <XIcon className="h-4" />
+                    ) : (
+                      ""
+                    )}
+                  </span>
+                  <p
+                    className="hover:bg-gray-100 flex-1 p-1 cursor-pointer rounded-sm transition duration-200 ease-out"
+                    onClick={() => handleBranch1Click("تفرع1")}
+                  >
+                    تفرع1
+                  </p>
+                </div>
+                <div className="flex items-center gap-x-2 ">
+                  <span
+                    ref={branch2}
+                    onClick={() => handleBranch2Click("تفرع2")}
+                    className={`w-4 h-4 rounded-sm  ${br1}  border border-gray-500 flex items-center justify-center cursor-pointer`}
+                  >
+                    {br1 === "bg-blue-500" ? (
+                      <CheckIcon className="h-4 text-white " />
+                    ) : br1 === "bg-red-500" ? (
+                      <XIcon className="h-4" />
+                    ) : (
+                      ""
+                    )}
+                  </span>
+                  <p
+                    className="hover:bg-gray-100 flex-1 p-1 cursor-pointer rounded-sm transition duration-200 ease-out"
+                    onClick={() => handleBranch2Click("تفرع2")}
+                  >
+                    تفرع2
+                  </p>
+                </div>
+                <div className="flex items-center gap-x-2 ">
+                  <span
+                    ref={branch3}
+                    onClick={() => handleBranch3Click("تفرع3")}
+                    className={`w-4 h-4 rounded-sm  ${br2}  border border-gray-500 flex items-center justify-center cursor-pointer`}
+                  >
+                    {br2 === "bg-blue-500" ? (
+                      <CheckIcon className="h-4 text-white " />
+                    ) : br2 === "bg-red-500" ? (
+                      <XIcon className="h-4" />
+                    ) : (
+                      ""
+                    )}
+                  </span>
+                  <p
+                    className="hover:bg-gray-100 flex-1 p-1 cursor-pointer rounded-sm transition duration-200 ease-out"
+                    onClick={() => handleBranch3Click("تفرع3")}
+                  >
+                    تفرع3
+                  </p>
+                </div>
+              </div>
+            </Transition>
+          </div>
 
           <Tree
             handleChange={handleSeason}
@@ -371,10 +426,18 @@ function Filters() {
             placeholder=" المواسم"
             data={seasonData}
           />
-          <RangePicker placeholder={["البداية","النهاية"]} onChange={handleDateChange}  />
+          <RangePicker
+            placeholder={["البداية", "النهاية"]}
+            onChange={handleDateChange}
+          />
         </div>
         <div className="text-center my-10 overflow-hidden rounded-lg">
-          <Image src="/map.jpg" height={500} width={800} className="hover:scale-110 transition duration-300 rounded-lg"/>
+          <Image
+            src="/map.jpg"
+            height={500}
+            width={800}
+            className="hover:scale-110 transition duration-300 rounded-lg"
+          />
         </div>
         <div className="text-center mb-5">
           <button
@@ -395,13 +458,12 @@ function Filters() {
           {show && <Results />}
 
           <div className="flex items-center justify-center">
-             {show && (
-            <button className="outline-none my-10 bg-green-500 text-white px-5 py-2 rounded-md transition duration-200 hover:bg-green-400 hover:shadow-lg">
-              تصدير كملف Excel{" "}
-            </button>
-          )}
+            {show && (
+              <button className="outline-none my-10 bg-green-500 text-white px-5 py-2 rounded-md transition duration-200 hover:bg-green-400 hover:shadow-lg">
+                تصدير كملف Excel{" "}
+              </button>
+            )}
           </div>
-         
         </div>
       </div>
     </div>
